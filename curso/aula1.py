e50a1ca44e0adc10d846defb9fa907bc3372c6d0
unknown <dueduardo97@hotmail.com>
2021-12-17 23:27:47 -0300

    salvando codigo

--- curso/aula1.py
+++ curso/aula1.py
@@ -1,1 +1,14 @@
-print('ola mundo')
+print("###################")
+print("ADIVINHE O NUMERO")
+print("###################")
+
+num = '10'
+
+while True:
+    usuario = input('Digite seu numero: ')
+
+    if(num == usuario):
+        print('Voce acertou!!')
+        break
+    else:
+        print('Errou!')
